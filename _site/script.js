427116f1a3492162ddc66dc6a7ec69859008fefb
Odrian OP <boodev22@gmail.com>
2018-03-24 19:56:26 -0500

    1st

---
+++ _site/script.js
@@ -0,0 +1,13 @@
+var options = {
+  valueNames: [ 'name' ]
+};
+
+var userList = new List('products', options);
+
+$(function () {
+  $('[data-toggle="popover"]').popover({
+      trigger: 'hover'
+    
+      
+        });
+    });
